a7b9e223ffd23fc57c029be9951afa17136e808a
alexi <alexihampson8@gmail.com>
2022-08-26 12:21:44 +0100

    Completes Ticket 19

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SingleArticle from "./components/articleId/SingleArticle";
 import NotFound from "./components/notFound/NotFound";
 import TopicList from "./components/topics/TopicList";
 import UserPage from "./components/userPage/UserPage";
+import UserList from "./components/users/UserList";
 import { UserContext } from "./context/user";
 import { useState } from "react";
 import { ErrContext } from "./context/err";
@@ -28,6 +29,7 @@ function App() {
             <Route path="/articles/:article_id" element={<SingleArticle />} />
             <Route path="/topics" element={<TopicList />} />
             <Route path="/users/:username" element={<UserPage />} />
+            <Route path="/users" element={<UserList />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
--- src/components/topics/TopicCard.jsx
+++ src/components/topics/TopicCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const TopicCard = ({ topic }) => {
   return (
     <Link to={`/topics/${topic.slug}`}>
-      <li className="border-2 border-white rounded m-4 p-2 sm:basis-20">
+      <li className="border-2 border-white rounded m-4 p-2 sm:basis-20 shadow shadow-sky-800 sm:hover:shadow-lg sm:hover:shadow-sky-800">
         <div className="grid grid-rows-2">
           <span className="capitalize row-auto text-xl font-bold p-1 m-3">{topic.slug}</span>
           <span className="row-auto">{topic.description}</span>
